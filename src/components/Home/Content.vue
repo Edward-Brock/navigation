4c2395ae3b6decb171a975c001c27f6500fd6e54
DESKTOP-4DBQPSO\14164 <1416477069@qq.com>
2023-07-04 15:33:11 +0800

    更新特性 - 更新 version 版本 - 优化 针对网站 Logo 显示布局的修改

--- src/components/Home/Content.vue
+++ src/components/Home/Content.vue
@@ -48,17 +48,19 @@ onMounted(() => {
       <div class="grid grid-cols-2 gap-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
         <template v-for="site in categoryGroup['sites']">
           <div
-              class="group relative p-3 border-2 border-transparent hover:border-gray-100 rounded-md bg-transparent hover:bg-gray-50 no-underline cursor-pointer"
+              class="group p-3 border-2 border-transparent hover:border-gray-100 rounded-md bg-transparent hover:bg-gray-50 no-underline cursor-pointer"
               @click="refreshTo(site)">
-            <!-- 网站 Logo -->
-            <div
-                class="absolute top-1 right-2 w-9 h-9 flex justify-center items-center rounded-full overflow-hidden opacity-0 group-hover:opacity-100">
-              <img :src="site.logo ? site.logo : defaultLogoUrl" :alt="site.name">
-            </div>
-            <!-- 网站名称 -->
-            <div
-                class="font-bold text-gray-400 mb-2 w-3/4 group-hover:underline decoration-wavy decoration-gray-950 group-hover:text-gray-950">
-              {{ site['name'] }}
+            <div class="flex flex-row items-center mb-2">
+              <!-- 网站名称 -->
+              <div
+                  class="font-bold text-gray-400 w-3/4 group-hover:underline decoration-wavy decoration-gray-950 group-hover:text-gray-950">
+                {{ site['name'] }}
+              </div>
+              <!-- 网站 Logo -->
+              <div
+                  class="w-9 h-9 flex justify-center items-center rounded-full overflow-hidden opacity-0 group-hover:opacity-100">
+                <img :src="site.logo ? site.logo : defaultLogoUrl" :alt="site.name">
+              </div>
             </div>
             <!-- 网站描述 -->
             <div class="text-sm text-gray-400 truncate" :title="site['description']">{{ site['description'] }}</div>
