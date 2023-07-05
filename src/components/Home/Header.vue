66a5c8ee77daa86b12d8d81c47038c28fbd1d588
DESKTOP-4DBQPSO\14164 <1416477069@qq.com>
2023-07-05 22:38:59 +0800

    更新特性 - 更新 version 版本 - 优化 Header 模块 GitHub 样式

--- src/components/Home/Header.vue
+++ src/components/Home/Header.vue
@@ -31,7 +31,7 @@ function toGitHub(data: string) {
           <!-- GitHub 项目跳转-->
           <div
               class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 cursor-pointer hidden-xs-only">
-            <div class="flex flex-col items-center mx-2" @click="toGitHub('q')">
+            <div class="group flex flex-col items-center mx-2" @click="toGitHub('q')">
               <button type="button"
                       class="rounded-full p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-1 focus:ring-white focus:ring-offset-1 focus:ring-offset-gray-800"
               >
@@ -42,9 +42,9 @@ function toGitHub(data: string) {
                       d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
                 </svg>
               </button>
-              <div class="text-xs">前端代码</div>
+              <div class="text-[10px] opacity-0 group-hover:text-gray-500 group-hover:opacity-100">前端代码</div>
             </div>
-            <div class="flex flex-col items-center mx-2" @click="toGitHub('h')">
+            <div class="group flex flex-col items-center mx-2" @click="toGitHub('h')">
               <button type="button"
                       class="rounded-full p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-1 focus:ring-white focus:ring-offset-1 focus:ring-offset-gray-800"
               >
@@ -55,7 +55,9 @@ function toGitHub(data: string) {
                       d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
                 </svg>
               </button>
-              <div class="text-xs">服务端代码</div>
+              <div class="text-[10px] opacity-0 group-hover:text-gray-500 group-hover:opacity-100">
+                服务端代码
+              </div>
             </div>
           </div>
         </div>
