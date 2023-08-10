4168af0f5bec17be405f373d71dedb3eb1eacc29
Edward-Brock <1416477069@qq.com>
2023-08-10 22:12:04 +0800

    - 更新 package.json 版本号； - 修复搜索引擎列表中 Wikipedia id 的错误大写； - 添加搜索框在用户选择搜索引擎后的记忆功能，清理 localStorage 前将持续有效；

--- src/components/Home/Search.vue
+++ src/components/Home/Search.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {reactive} from "vue";
+import {onMounted, reactive} from "vue";
 
 /**
  * 搜索引擎列表
@@ -8,7 +8,7 @@ let searchSelectArray = [
   {id: "baidu", name: "百度", url: `https://www.baidu.com/s?ie=UTF-8&wd=`},
   {id: "google", name: "Google", url: `https://www.google.com/search?q=`},
   {id: "bing", name: "Bing", url: `https://www.bing.com/search?q=`},
-  {id: "Wikipedia", name: "Wikipedia", url: "https://zh.wikipedia.org/zh-cn/"},
+  {id: "wikipedia", name: "Wikipedia", url: "https://zh.wikipedia.org/zh-cn/"},
   {id: "duckduckgo", name: "DuckDuckGo", url: "https://duckduckgo.com/?q="}
 ]
 
@@ -23,19 +23,42 @@ let searchInfo = reactive({
 })
 
 /**
+ * 监听搜索引擎切换
+ * select 切换时自动切换数据
+ */
+function searchSelectHandleChange() {
+  let search = searchSelectArray.find((searchArray: any) => searchArray.id === searchInfo.searchSelect)
+  localStorage.setItem("searchSelect", JSON.stringify(search));
+  console.log(`搜索引擎数据已切换至 ${search?.name} 并保存`)
+}
+
+/**
  * 用户点击按钮或按下 Enter 进行搜索
  */
 function toSearch() {
-  let search = searchSelectArray.find((searchArray: any) => searchArray.id === searchInfo.searchSelect)?.url
-  window.open(search + searchInfo.keyword)
+  let search = searchSelectArray.find((searchArray: any) => searchArray.id === searchInfo.searchSelect)
+  window.open(search?.url + searchInfo.keyword)
+  searchInfo.keyword = ""
 }
+
+onMounted(() => {
+  /**
+   * 判断 localStorage 中是否存在搜索引擎选择记录
+   * 如果存在则自动切换到存储的搜索引擎
+   */
+  if (localStorage.getItem("searchSelect")) {
+    console.log("搜索引擎数据存储存在")
+    const searchSelect = localStorage.getItem("searchSelect") as string
+    searchInfo.searchSelect = JSON.parse(searchSelect)?.id
+  }
+})
 </script>
 
 <template>
   <div class="relative my-8">
     <div class="absolute inset-y-0 left-0 flex items-center">
       <label for="search" class="sr-only">Search Engineer</label>
-      <select id="Search" name="search-select" v-model="searchInfo.searchSelect"
+      <select id="Search" name="search-select" v-model="searchInfo.searchSelect" @change="searchSelectHandleChange"
               class="h-12 rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-4 text-gray-400 focus:ring-1 focus:ring-inset focus:ring-gray-600 sm:text-sm">
         <option v-for="item in searchSelectArray" :key="item.id" :value="item.id">{{ item.name }}</option>
       </select>
