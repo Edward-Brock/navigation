df02f22372769a13cf8f42a627ce64266940df5f
Edward Brock <1416477069@qq.com>
2025-02-16 23:55:46 +0800

    feat: 安装 @headlessui/vue 依赖 feat: 安装 @heroicons/vue 依赖 feat: 安装 valibot 依赖 feat: 添加 AppHeader 组件

---
+++ app/components/AppHeader.vue
@@ -0,0 +1,47 @@
+<template>
+  <Disclosure>
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div class="relative flex h-14 items-center justify-between">
+        <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+          <nuxt-link to="/">
+            <div class="flex shrink-0 items-center">
+              <img
+                class="h-8 w-auto"
+                src="https://tailwindui.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
+                alt="Your Company"
+              >
+            </div>
+          </nuxt-link>
+        </div>
+        <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+          <template v-if="!session.data">
+            <UButton
+              class="rounded-full"
+              trailing-icon="i-lucide-arrow-right"
+              color="neutral"
+              variant="ghost"
+              size="lg"
+              to="/auth/login"
+            >
+              登录
+            </UButton>
+          </template>
+          <template v-else>
+            <UserDropdown />
+          </template>
+        </div>
+      </div>
+    </div>
+  </Disclosure>
+</template>
+
+<script setup lang="ts">
+import { Disclosure } from '@headlessui/vue'
+import { authClient } from '~/utils/auth-client'
+
+const session = authClient.useSession()
+</script>
+
+<style scoped>
+
+</style>
---
+++ app/pages/auth/login.vue
@@ -0,0 +1,135 @@
+<!-- app/pages/auth/login.vue -->
+<template>
+  <main class="flex min-h-svh flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+        登录你的账号
+      </h2>
+    </div>
+
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <UForm
+        :schema="v.safeParser(schema)"
+        :state="state"
+        class="space-y-6"
+        @submit="onSubmit"
+      >
+        <UFormField
+          label="电子邮箱"
+          name="email"
+          required
+          size="lg"
+          class="block text-sm/6 font-medium text-gray-900"
+        >
+          <UInput
+            v-model="state.email"
+            type="email"
+            autocomplete="email"
+            placeholder="输入电子邮箱"
+            icon="i-lucide-mail"
+            class="block w-full text-base text-gray-900 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+          />
+        </UFormField>
+
+        <UFormField
+          label="密码"
+          name="password"
+          required
+          size="lg"
+          class="block text-sm/6 font-medium text-gray-900"
+        >
+          <UInput
+            v-model="state.password"
+            type="password"
+            autocomplete="current-password"
+            placeholder="输入密码"
+            icon="i-lucide-lock"
+            class="block w-full text-base text-gray-900 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+          />
+        </UFormField>
+
+        <UCheckbox
+          v-model="state.rememberMe"
+          color="neutral"
+          label="在此设备上记住我"
+          name="rememberMe"
+        />
+
+        <UButton
+          color="neutral"
+          type="submit"
+          size="lg"
+          :loading="isSubmitting"
+          :disabled="isLoggedIn"
+          class="block w-full font-bold"
+        >
+          登录
+        </UButton>
+      </UForm>
+    </div>
+  </main>
+</template>
+
+<script lang="ts" setup>
+import * as v from 'valibot'
+import type { FormSubmitEvent } from '#ui/types'
+
+definePageMeta({
+  layout: 'single',
+})
+
+const schema = v.object({
+  email: v.pipe(v.string(), v.trim(), v.email('请输入电子邮箱')),
+  password: v.pipe(v.string(), v.trim(), v.minLength(8, '必须至少包含 8 个字符'), v.maxLength(32, '最多不超过 32 个字符')),
+  rememberMe: v.pipe(v.boolean()),
+})
+
+type Schema = v.InferOutput<typeof schema>
+
+const isSubmitting = ref(false)
+const isLoggedIn = ref(false)
+const state = reactive({
+  email: '',
+  password: '',
+  rememberMe: true,
+})
+
+const toast = useToast()
+const router = useRouter()
+async function onSubmit(event: FormSubmitEvent<Schema>) {
+  isSubmitting.value = true // 在提交时设置为 true
+  try {
+    await authClient.signIn.email({
+      email: event.data.email,
+      password: event.data.password,
+      rememberMe: event.data.rememberMe,
+    }, {
+      onSuccess: () => {
+        // 登录成功后将 isLoggedIn 设置为 true
+        isLoggedIn.value = true
+        toast.add({
+          icon: 'i-heroicons-check-circle',
+          title: '登录成功',
+          color: 'success',
+        })
+        router.push('/dashboard')
+      },
+      onError: (ctx) => {
+        toast.add({
+          icon: 'i-heroicons-x-circle',
+          title: '登录失败',
+          description: ctx.error.message,
+          color: 'error',
+        })
+      },
+    })
+  }
+  finally {
+    isSubmitting.value = false // 提交完成后重置为 false
+  }
+}
+</script>
+
+<style>
+
+</style>
