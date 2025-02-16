393ff2a09a9c0e3658f4553c9d9009a70bbcdf35
Edward Brock <1416477069@qq.com>
2025-02-16 23:43:59 +0800

    feat: 添加 default 布局 feat: 添加 single 布局

---
+++ app/layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <header>
+      <AppHeader />
+    </header>
+
+    <main>
+      <nuxt-page />
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
