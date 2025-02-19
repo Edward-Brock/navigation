e0bd95bb0bf8dc71ac6226878039628046770f8e
Edward Brock <1416477069@qq.com>
2025-02-19 21:19:39 +0800

    fix: 修复错误的分类信息获取方式，通过检查 Option 表中 `default_user_id` 值显示网站默认分类及相关网站信息

--- server/api/category/index.get.ts
+++ server/api/category/index.get.ts
@@ -1,23 +1,27 @@
 // server/api/category/index.get.ts
-import { auth } from '~/utils/auth'
 import prisma from '~/utils/prisma'
 
-export default defineEventHandler(async (event) => {
-  const session = await auth.api.getSession({
-    headers: event.headers,
-  })
-
-  if (!session || !session.user) {
-    throw createError({ statusCode: 401, message: 'User not authenticated' })
-  }
-
-  const userId = session.user.id
-
+export default defineEventHandler(async () => {
   try {
+    // 查询 Option 表中的 'default_user_id' 选项
+    const defaultUserIdOption = await prisma.option.findUnique({
+      where: {
+        name: 'default_user_id', // 根据 name 查找标识为 default_user_id 的记录
+      },
+    })
+
+    // 如果没有找到该记录，返回错误或使用默认用户ID
+    if (!defaultUserIdOption || !defaultUserIdOption.value) {
+      throw createError({ statusCode: 500, message: 'Default User ID not set in option' })
+    }
+
+    // 获取默认用户ID
+    const defaultUserId = defaultUserIdOption.value
+
     // 查询所有属于该用户的分类，并排除已删除的分类
     const categories = await prisma.category.findMany({
       where: {
-        userId,
+        userId: defaultUserId,
         deletedAt: null,
       },
       orderBy: [{
