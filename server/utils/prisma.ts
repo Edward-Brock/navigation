470f91d8c6c8ba92daf6859ff5bb805137f5f07e
Edward Brock <1416477069@qq.com>
2025-08-03 14:39:25 +0800

    chore: 添加 prisma 依赖 chore: 添加 @prisma/client 依赖

---
+++ server/utils/prisma.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from '~~/generated/prisma'
+
+declare global {
+  var _prismaClient: PrismaClient | undefined
+}
+
+const prisma: PrismaClient = globalThis._prismaClient ?? new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') {
+  globalThis._prismaClient = prisma
+}
+
+export default prisma
